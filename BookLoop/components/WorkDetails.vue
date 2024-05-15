70f9fc1ea3aec0be360fe1baa9684f54d159e6fb
Victoria <martinezvictoria781@gmail.com>
2024-05-15 01:26:08 +0100

    minor fix

--- BookLoop/components/WorkDetails.vue
+++ BookLoop/components/WorkDetails.vue
@@ -8,7 +8,7 @@
         <h2 class="text-m title-font text-gray-500 tracking-widest"v-if="work.author">{{ work.author }}</h2>
         <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{ work.originalTitle }}</h1>
         <div class="flex mb-4">
-          <span class="flex items-center">
+          <span class="flex items-center" v-if="work.reviews.length > 0">
             <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
             </svg>
@@ -24,7 +24,10 @@
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
             </svg>
-            <span class="text-gray-600 ml-3">4 Reviews</span>
+            <span class="text-gray-600 ml-3">{{work.reviews.length}} Reviews</span>
+          </span>
+          <span v-else>
+            <span class="text-gray-600 ml-3">{{work.reviews.length}} Reviews</span>
           </span>
 
         </div>
