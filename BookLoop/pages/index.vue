0de88a9bda9822c49751872c5162fae1673d686c
Victoria <martinezvictoria781@gmail.com>
2024-06-25 03:43:11 +0100

    starting landing page

--- BookLoop/pages/index.vue
+++ BookLoop/pages/index.vue
@@ -1,5 +1,96 @@
 <template>
-    Welcome to Book Loop!
+<div class="bg-white">
+  <div class="mx-auto max-w-2xl px-4 py-6 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+
+    <div class="mt-12 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+      <div class="group relative">
+        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden  bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+          <img src="https://d2w9rnfcy7mm78.cloudfront.net/7943932/original_7cc65dde5bf192012b09556f9780783e.jpg?1594300086?bc=0" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <div>
+            <h3 class="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Discover New Reads 
+              </a>
+            </h3>
+            <p class="text-sm font-medium text-gray-900">Some other text</p>
+          </div>
+          <p class="text-sm font-medium text-gray-900">- Find New Books Online</p>
+        </div>
+      </div>
+
+      <div class="group relative">
+        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden  bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+          <img src="https://d2w9rnfcy7mm78.cloudfront.net/12385119/original_4a51f22545c5aa6c4204c0403ae78eb6.png?1624904704?bc=0" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <div>
+            <h3 class="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Discover New Reads 
+              </a>
+            </h3>
+            <p class="text-sm font-medium text-gray-900">Some other text</p>
+          </div>
+          <p class="text-sm font-medium text-gray-900">- Find New Books Online</p>
+        </div>
+      </div>
+
+      <div class="group relative">
+        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden  bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+          <img src="https://d2w9rnfcy7mm78.cloudfront.net/27634058/original_d53b0e49989af4d9628188a39f34248f.jpg?1713447123?bc=0" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <div>
+            <h3 class="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Declutter your home and Get rid of your old books
+              </a>
+            </h3>
+            
+          </div>
+        </div>
+      </div>
+
+      <div class="group relative">
+        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden  bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+          <img src="https://d2w9rnfcy7mm78.cloudfront.net/27087635/original_5a56d792dae16bede6fb25aad886b547.jpg?1710930248?bc=0" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <div>
+            <h3 class="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Discover New Reads 
+              </a>
+            </h3>
+            <p class="text-sm font-medium text-gray-900">Some other text</p>
+          </div>
+          <p class="text-sm font-medium text-gray-900">- Find New Books Online</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  <div class="flex">
+    <div class="w-1/2 h-30 bg-gray-200">
+    <img src="https://d2w9rnfcy7mm78.cloudfront.net/22692994/original_4eec30fe3cde3f4b93182155a2d99834.jpg?1689599849?bc=0"  class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+  </div>
+  <div class="w-1/2 flex flex-col justify-between">
+            <div class="h-20">
+                <img src="https://images.are.na/eyJidWNrZXQiOiJhcmVuYV9pbWFnZXMiLCJrZXkiOiIxNjA3NDg4Ni9vcmlnaW5hbF85Yzk0MTJhYjFmMTE4NTgxM2YzMzdkYWYyZDA1NTNmZC5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjEyMDAsImhlaWdodCI6MTIwMCwiZml0IjoiaW5zaWRlIiwid2l0aG91dEVubGFyZ2VtZW50Ijp0cnVlfSwid2VicCI6eyJxdWFsaXR5Ijo3NX0sImpwZWciOnsicXVhbGl0eSI6NzV9LCJyb3RhdGUiOm51bGx9fQ==" class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+            </div>
+            <div class="text-top p-1 mx-2">
+                <p clas="top-0">a curated haven for the discerning reader. We offer a confluence of esteemed second-hand finds and the thrill of discovering new releases, all woven together by a vibrant community of fellow literature enthusiasts.</p>
+            </div>
+        </div>
+    </div>
+
+</div>
 </template>
 
 <script setup>
@@ -8,6 +99,11 @@ import 'flowbite/dist/flowbite.min.css';
 
 </script>
 
-<style lang="scss" scoped>
-
+<style  scoped>
+        .h-30 {
+            height: 750px
+        }
+        .h-20 {
+            height: 600px; /* Smaller height for the right container */
+        }
 </style>
